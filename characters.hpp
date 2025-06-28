112ac6eda93b68c64878eb1e6f4b7a884c6f0122
Andrius <guerramontoyaandrius288@gmail.com>
2025-06-28 19:50:25 +0100

    Added a basic logic and system of inventory and weaponds

--- characters.hpp
+++ characters.hpp
@@ -1,14 +1,65 @@
 #include <iostream>
+#include <vector>
 
 typedef std::string str_t;
+template<typename T>
+using vec_t = std::vector<T>;
+
+
+class Object {
+private:
+    str_t name;
+    int value;
+public:
+    Object(str_t objectName, int objectValue = 1) {
+        name = objectName;
+        value = objectValue;
+    }
+    str_t get_name() const {
+        return name;
+    }
+    int get_value() const {
+        return value;
+    }
+protected:
+    void set_value(int newValue) {
+        value = newValue;
+    }
+};
+
+class Weapon : public Object {
+private:
+    int damage;
+public:
+    Weapon(str_t weaponName, int weaponDamage)
+        : Object(weaponName, 1), damage(weaponDamage) {
+    }
+    int get_damage() const {
+        return damage;
+    }
+};
+
+
 
 class Character {
 private:
     str_t name;
     int health = 100;
+    vec_t<Object*> inventory; // Example of using the template alias
+    Weapon *equippedWeapon = nullptr; // Pointer to a Weapon object
 public:
     Character(str_t characterName) {
         name = characterName;
+    }
+    ~Character() {
+        for (Object* obj : inventory) {
+            std::cout << name << " is cleaning up " << obj->get_name() << " from inventory.\n";
+            delete obj; // Clean up dynamically allocated objects
+        }
+        if (equippedWeapon) {
+            std::cout << name << " is cleaning up " << equippedWeapon->get_name() << " from inventory.\n";
+            delete equippedWeapon; // Clean up the equipped weapon if it exists
+        }
     }
     void get_attacked(int damage) {
         health -= damage;
@@ -18,6 +69,9 @@ public:
         std::cout << name << " has been attacked! Health is now: " << health << '\n';
     }
     void attack(Character &target, int damage = 10) {
+        if (equippedWeapon) {
+            damage = equippedWeapon->get_damage();
+        }
         std::cout << name << " attacks " << target.name << " for " << damage << " damage!\n";
         target.get_attacked(damage);
     }
@@ -26,5 +80,17 @@ public:
     }
     int get_health() {
         return health;
+    }
+    void add_to_inventory(Object *object) {
+        inventory.push_back(object);
+        std::cout << name << " added " << object->get_name() << " to inventory.\n";
+    }
+    void equip_weapon(Weapon *weapon) {
+        if (equippedWeapon) {
+            std::cout << name << " unequipped " << equippedWeapon->get_name() << ".\n";
+            delete equippedWeapon; // Clean up the previous weapon
+        }
+        equippedWeapon = weapon;
+        std::cout << name << " equipped " << weapon->get_name() << " with damage " << weapon->get_damage() << ".\n";
     }
 };
--- main.cpp
+++ main.cpp
@@ -6,14 +6,19 @@ typedef std::string str_t;
 int main()
 {
     std::cout << "Welcome to CppRPG!" << '\n';
+    std::cout << "==================" << '\n';
 
     Character hero("Hero");
     Character monster("Monster");
+    Weapon *sword = new Weapon("Sword", 20);
+
+    hero.equip_weapon(sword);
 
     monster.attack(hero);
     hero.attack(monster);
+
     std::cout << monster.get_name() << " is angry!" << '\n';
     monster.attack(hero, 90);
-
+    
     return 0;
 }
